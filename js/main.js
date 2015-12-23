72e3030a2a0400d652e065df2b01d37a2a20c887
Christina Deemer <cahdeemer@gmail.com>
2015-12-23 15:31:01 -0500

    styling homepage building search thing

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 $(document).ready(function(){
 
-  $('.homepage-slider').slick({
+  $('.jumbotron-slider').slick({
   	fade: true,
   	arrows: false,
     dots: true
+  });
+
+  $('.search-input-container').hide();
+
+  $(function() {
+  	$('.search-btn').click(function(event) {
+  		event.preventDefault();
+  		$('.search-input-container').show().addClass('search-input-show');
+  		$(this).addClass('search-btn-move');
+  		$('.navbar-text').hide();
+  	});
+  });
+
+  $(function() {
+  	$('.reset-btn').click(function(event) {
+  		event.preventDefault();
+  		$('.search-input-container').hide().removeClass('search-input-show');
+  		$('.search-btn').removeClass('search-btn-move');
+  		$('.navbar-text').show();
+  	});
   });
   
 });
