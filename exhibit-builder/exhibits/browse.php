fedf361f57c3f7a8d6604e9568a9922a00c2c6dd
Christina Deemer <cahdeemer@gmail.com>
2016-03-04 14:19:34 -0500

    exhibits browse page

--- exhibit-builder/exhibits/browse.php
+++ exhibit-builder/exhibits/browse.php
@@ -4,51 +4,54 @@ echo head(array('title' => $title, 'bodyclass' => 'exhibits browse'));
 ?>
 
 
-
-
-<h1><?php echo $title; ?> <?php echo __('(%s total)', $total_results); ?></h1>
-<?php if (count($exhibits) > 0): ?>
-
 <main class="stories-page">
 		<div class="jumbotron stories-jumbotron">
 			<div class="placeholder-1">
+				<?php foreach (loop('exhibits') as $exhibit): ?>
+				<?php if($exhibit->featured == true): ?>
 				<div class="jumbotron-slider-img jumbotron-img-stories">
-					<img src="http://i.ebayimg.com/00/s/MTUyNFgxNjAw/z/MbIAAOSwu4BVxJpm/$_20.JPG">
+					<?php $featuredExhibitImage = sckls_exhibit_builder_get_first_image_html($exhibit); ?>
+					<?php echo '<img src="'. $featuredExhibitImage . '">'; ?>
 				</div>
 				<section class="stories-jumbotron-slider-text">
 					<h4 class="stories-jumbotron-slider-text-subheading">Featured Story</h4>
-					<h2 class="jumbotron-slider-text-featured-story">The Berwind Mausoleum</h2>
-					<p class="stories-jumbotron-summary">Hidden in West Laurel Hill Cemetary just outside Philadelphia, an octagonal mausoleum recalls the Tower of the Winds in Athens, and creates an architectural pun on the name of its occupants.</p>
-					<a href="" class="jumbotron-slider-text-link"><h4>View the Story</h4></a>
-				</section>
+					<h2 class="jumbotron-slider-text-featured-story">
+						<?php $featuredExhibitTitle = metadata($exhibit, 'title'); ?>
+						<?php echo($featuredExhibitTitle); ?>
+					</h2>
+					<p class="stories-jumbotron-summary">
+						<?php $featuredExhibitDescription = metadata($exhibit, 'description'); ?>
+						<?php echo strip_formatting($featuredExhibitDescription); ?>
+					</p>
+					<?php echo link_to_exhibit('<h4>View the Story</h4>', array('class' => 'jumbotron-slider-text-link')); ?>													</section>
+				<?php endif; ?>
+				<?php endforeach; ?>
 			</div>
 		</div> <!-- /jumbotron -->
+		
+		<div class="container">
+			<?php foreach (loop('exhibit') as $exhibit): ?>
+			<div class="col-sm-6 stories-thumbnail">
+				<div class="thumbnail">
+					<div class="thumbnail-img">
+					<?php $exhibitImage = sckls_exhibit_builder_get_first_image_html($exhibit); ?>
+					<?php echo '<img src="'. $exhibitImage . '">'; ?>
+					</div>
+					<div class="thumbnail-caption">
+						<?php $featuredExhibitTitle = metadata($exhibit, 'title'); ?>
+						<?php echo($featuredExhibitTitle); ?>
+					</div>
+						
+					
+				</div>
+			</div>
+			<?php endforeach; ?>
+		</div>
+		
 
 
 
 <?php echo pagination_links(); ?>
 
-<?php $exhibitCount = 0; ?>
-<?php foreach (loop('exhibit') as $exhibit): ?>
-    <?php $exhibitCount++; ?>
-    <div class="exhibit <?php if ($exhibitCount%2==1) echo ' even'; else echo ' odd'; ?>">
-        <h2><?php echo link_to_exhibit(); ?></h2>
-        <?php if ($exhibitImage = record_image($exhibit, 'square_thumbnail')): ?>
-            <?php echo exhibit_builder_link_to_exhibit($exhibit, $exhibitImage, array('class' => 'image')); ?>
-        <?php endif; ?>
-        <?php if ($exhibitDescription = metadata('exhibit', 'description', array('no_escape' => true))): ?>
-        <div class="description"><?php echo $exhibitDescription; ?></div>
-        <?php endif; ?>
-        <?php if ($exhibitTags = tag_string('exhibit', 'exhibits')): ?>
-        <p class="tags"><?php echo $exhibitTags; ?></p>
-        <?php endif; ?>
-    </div>
-<?php endforeach; ?>
-
-<?php echo pagination_links(); ?>
-
-<?php else: ?>
-<p><?php echo __('There are no exhibits available yet.'); ?></p>
-<?php endif; ?>
 
 <?php echo foot(); ?>
--- functions.php
+++ functions.php
@@ -6,3 +6,26 @@ function public_nav_main_bootstrap() {
     $nav->setPartial($partial);
     return $nav->render();
 }
+
+function sckls_exhibit_builder_get_first_image_html($exhibit) {
+	$html = '';
+    $count = 0;
+	if (!$exhibit) {
+        $exhibit = get_current_record('exhibit');
+    }
+    set_loop_records('exhibit_page', $exhibit->TopPages);
+    
+    foreach (loop('exhibit_page') as $exhibitPage) {
+        $attachments = $exhibitPage->getAllAttachments();
+        foreach ($attachments as $attachment):
+            if ($count === 0){
+                $item = $attachment->getItem();
+                $file = $attachment->getFile();
+    
+                $html .= file_display_url($file, 'fullsize');
+            }
+            $count++;
+        endforeach;
+    }
+    return $html;
+}
